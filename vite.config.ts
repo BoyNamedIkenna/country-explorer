aaad3f454ed0caf1dbf8bbc40923800e823968a2
Ikenna <127604100+BoyNamedIkenna@users.noreply.github.com>
2025-05-12 15:08:24 +0100

    `Update package.json and vite.config.ts to remove trailing slashes`

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/country-explorer/"
+  base: "/country-explorer",
 });
