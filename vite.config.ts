5d9f0c55e68ef418392e4f376dfb0f13e1d43345
Ikenna <127604100+BoyNamedIkenna@users.noreply.github.com>
2025-05-13 09:59:49 +0100

    `Update package.json and vite.config.ts to remove homepage and add VITE_BASE_PATH variable`

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/country-explorer/",
+  base:process.env.VITE_BASE_PATH || "/country-explorer"
 });
